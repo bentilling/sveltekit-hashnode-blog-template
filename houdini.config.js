71af95ee478e9c885cb2422168e985250055af40
Ben Tilling <54737272+bentilling@users.noreply.github.com>
2023-10-28 17:23:49 +0100

    set up houdini

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://gql.hashnode.com"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
